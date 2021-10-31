099dbe72c26ce2cc7d778a45fcbcdda6e5179ae2
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-10-31 10:49:59 +0100

    Add DPI components to list

--- src/components/ComponentsGraph.tsx
+++ src/components/ComponentsGraph.tsx
@@ -7,8 +7,72 @@ const COMPONENTS: Record<
 > = {
   DPI: [
     {
+      symbol: "UNI",
+      coingeckoApiId: "uniswap",
+    },
+    {
+      symbol: "AAVE",
+      coingeckoApiId: "aave",
+    },
+    {
+      symbol: "MKR",
+      coingeckoApiId: "maker",
+    },
+    {
+      symbol: "SUSHI",
+      coingeckoApiId: "sushi",
+    },
+    {
+      symbol: "COMP",
+      coingeckoApiId: "compound-governance-token",
+    },
+    {
+      symbol: "SNX",
+      coingeckoApiId: "havven",
+    },
+    {
       symbol: "YFI",
       coingeckoApiId: "yearn-finance",
+    },
+    {
+      symbol: "REN",
+      coingeckoApiId: "republic-protocol",
+    },
+    {
+      symbol: "LRC",
+      coingeckoApiId: "loopring",
+    },
+    {
+      symbol: "KNC",
+      coingeckoApiId: "kyber-network-crystal",
+    },
+    {
+      symbol: "BAL",
+      coingeckoApiId: "balancer",
+    },
+    {
+      symbol: "BADGER",
+      coingeckoApiId: "badger-dao",
+    },
+    {
+      symbol: "FARM",
+      coingeckoApiId: "harvest-finance",
+    },
+    {
+      symbol: "INST",
+      coingeckoApiId: "instadapp",
+    },
+    {
+      symbol: "CREAM",
+      coingeckoApiId: "cream-2",
+    },
+    {
+      symbol: "VSP",
+      coingeckoApiId: "vesper-finance",
+    },
+    {
+      symbol: "MTA",
+      coingeckoApiId: "meta",
     },
   ],
 };
