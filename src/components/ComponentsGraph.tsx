97614f1830b4b705f92b0653a36beee486dda56c
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-10-31 12:08:24 +0100

    Add responsive container

--- src/components/ComponentsGraph.tsx
+++ src/components/ComponentsGraph.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import {
   AreaChart,
   Area,
@@ -8,7 +8,7 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-} from "recharts";
+} from 'recharts'
 
 const COMPONENTS: Record<
   string,
@@ -16,151 +16,154 @@ const COMPONENTS: Record<
 > = {
   DPI: [
     {
-      symbol: "UNI",
-      coingeckoApiId: "uniswap",
+      symbol: 'UNI',
+      coingeckoApiId: 'uniswap',
     },
     {
-      symbol: "AAVE",
-      coingeckoApiId: "aave",
+      symbol: 'AAVE',
+      coingeckoApiId: 'aave',
     },
     {
-      symbol: "MKR",
-      coingeckoApiId: "maker",
+      symbol: 'MKR',
+      coingeckoApiId: 'maker',
     },
     {
-      symbol: "SUSHI",
-      coingeckoApiId: "sushi",
+      symbol: 'SUSHI',
+      coingeckoApiId: 'sushi',
     },
     {
-      symbol: "COMP",
-      coingeckoApiId: "compound-governance-token",
+      symbol: 'COMP',
+      coingeckoApiId: 'compound-governance-token',
     },
     {
-      symbol: "SNX",
-      coingeckoApiId: "havven",
+      symbol: 'SNX',
+      coingeckoApiId: 'havven',
     },
     {
-      symbol: "YFI",
-      coingeckoApiId: "yearn-finance",
+      symbol: 'YFI',
+      coingeckoApiId: 'yearn-finance',
     },
     {
-      symbol: "REN",
-      coingeckoApiId: "republic-protocol",
+      symbol: 'REN',
+      coingeckoApiId: 'republic-protocol',
     },
     {
-      symbol: "LRC",
-      coingeckoApiId: "loopring",
+      symbol: 'LRC',
+      coingeckoApiId: 'loopring',
     },
     {
-      symbol: "KNC",
-      coingeckoApiId: "kyber-network-crystal",
+      symbol: 'KNC',
+      coingeckoApiId: 'kyber-network-crystal',
     },
     {
-      symbol: "BAL",
-      coingeckoApiId: "balancer",
+      symbol: 'BAL',
+      coingeckoApiId: 'balancer',
     },
     {
-      symbol: "BADGER",
-      coingeckoApiId: "badger-dao",
+      symbol: 'BADGER',
+      coingeckoApiId: 'badger-dao',
     },
     {
-      symbol: "FARM",
-      coingeckoApiId: "harvest-finance",
+      symbol: 'FARM',
+      coingeckoApiId: 'harvest-finance',
     },
     {
-      symbol: "INST",
-      coingeckoApiId: "instadapp",
+      symbol: 'INST',
+      coingeckoApiId: 'instadapp',
     },
     {
-      symbol: "CREAM",
-      coingeckoApiId: "cream-2",
+      symbol: 'CREAM',
+      coingeckoApiId: 'cream-2',
     },
     {
-      symbol: "VSP",
-      coingeckoApiId: "vesper-finance",
+      symbol: 'VSP',
+      coingeckoApiId: 'vesper-finance',
     },
     {
-      symbol: "MTA",
-      coingeckoApiId: "meta",
+      symbol: 'MTA',
+      coingeckoApiId: 'meta',
     },
   ],
-};
+}
 
-const DAYS = 30;
+const DAYS = 30
 
 async function getMarketCapData(id: string) {
-  const apiUrl = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${DAYS}&interval=daily`;
-  const response = await axios.get(apiUrl);
+  const apiUrl = `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=${DAYS}&interval=daily`
+  const response = await axios.get(apiUrl)
 
-  const marketCapsUSD = response.data.market_caps.map(
-    (arr: number[]) => arr[1]
-  );
-  return marketCapsUSD;
+  const marketCapsUSD = response.data.market_caps.map((arr: number[]) => arr[1])
+  return marketCapsUSD
 }
 
 function ComponentsGraph(props: { name: string }) {
   const dates = Array.from({ length: DAYS + 1 }, (_, index) => {
     return {
       date: new Date(new Date().setDate(new Date().getDate() - (DAYS - index))),
-    };
-  });
+    }
+  })
   const [marketCapData, setMarketCapData] =
-    useState<Array<Record<string, any>>>(dates);
+    useState<Array<Record<string, any>>>(dates)
   useEffect(() => {
     for (const { symbol, coingeckoApiId } of COMPONENTS[props.name]) {
       getMarketCapData(coingeckoApiId).then((marketCapsUSD) => {
-        const newMarketCapData = marketCapData;
+        const newMarketCapData = marketCapData
         marketCapsUSD.forEach((value: number, i: number) => {
-          newMarketCapData[i][symbol] = value;
-        });
+          newMarketCapData[i][symbol] = value
+        })
 
-        setMarketCapData(newMarketCapData);
-      });
+        setMarketCapData(newMarketCapData)
+      })
     }
-  });
+  })
 
   return (
-    <div className="ComponentsGraph">
+    <div className='ComponentsGraph'>
       <h1>{props.name}</h1>
-      <AreaChart
-        width={500}
-        height={400}
-        data={marketCapData}
-        margin={{
-          top: 10,
-          right: 30,
-          left: 0,
-          bottom: 0,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Area
-          type="monotone"
-          dataKey="AAVE"
-          stackId="1"
-          stroke="#8884d8"
-          fill="#8884d8"
-        />
-        <Area
-          type="monotone"
-          dataKey="UNI"
-          stackId="1"
-          stroke="#82ca9d"
-          fill="#82ca9d"
-        />
-        <Area
-          type="monotone"
-          dataKey="YFI"
-          stackId="1"
-          stroke="#ffc658"
-          fill="#ffc658"
-        />
-      </AreaChart>
+      <ResponsiveContainer width='100%' aspect={3}>
+        <AreaChart
+          width={500}
+          height={400}
+          data={marketCapData}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis
+            dataKey='date'
+            tickFormatter={(value) => value.toLocaleDateString()}
+          />
+          <YAxis />
+          <Tooltip />
+          <Area
+            type='monotone'
+            dataKey='AAVE'
+            stackId='1'
+            stroke='#8884d8'
+            fill='#8884d8'
+          />
+          <Area
+            type='monotone'
+            dataKey='UNI'
+            stackId='1'
+            stroke='#82ca9d'
+            fill='#82ca9d'
+          />
+          <Area
+            type='monotone'
+            dataKey='YFI'
+            stackId='1'
+            stroke='#ffc658'
+            fill='#ffc658'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
-  );
+  )
 }
 
-export default ComponentsGraph;
+export default ComponentsGraph
